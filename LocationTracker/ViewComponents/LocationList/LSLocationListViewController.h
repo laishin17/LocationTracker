0f7f74271eef53b58c38e21d71fcba32f6e3e2de
laishin17 <laishin17@gmail.com>
2015-02-15 14:38:00 +0900

    位置情報を一覧表示するマンを作成

---
+++ LocationTracker/ViewComponents/LocationList/LSLocationListViewController.h
@@ -0,0 +1,14 @@
+//
+//  LSLocationListViewController.h
+//  LocationTracker
+//
+//  Created by laishin on 2015/02/15.
+//  Copyright (c) 2015年 laishin17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LSLocationListViewController : UITableViewController
+
+@end
---
+++ LocationTracker/ViewComponents/LocationList/LSLocationListViewController.m
@@ -0,0 +1,120 @@
+//
+//  LSLocationListViewController.m
+//  LocationTracker
+//
+//  Created by laishin on 2015/02/15.
+//  Copyright (c) 2015年 laishin17. All rights reserved.
+//
+
+#import "LSLocationListViewController.h"
+
+#import <CoreLocation/CoreLocation.h>
+#import "LSLocationManager.h"
+#import "GVUserDefaults+LSProperties.h"
+
+
+static NSString *const kLocationCellId = @"locationCellId";
+static const CGFloat kDefaultCellHeight = 50.0;
+
+
+@interface LSLocationListViewController ()
+
+@property (nonatomic) NSArray *locations;
+
+@end
+
+
+@implementation LSLocationListViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.clearsSelectionOnViewWillAppear = NO;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    GVUserDefaults *defaults = [GVUserDefaults standardUserDefaults];
+    self.locations = defaults.locations;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didUpdateLocation:)
+                                                 name:kLSLocationManagerDidUpdateNotification
+                                               object:nil];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    NSError *error = [[LSLocationManager sharedManager] startUpdatingLocation];
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"確認"
+                                                        message:error.localizedDescription
+                                                       delegate:nil
+                                              cancelButtonTitle:@"閉じる"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:kLSLocationManagerDidUpdateNotification
+                                                  object:nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Accessors
+#pragma mark - Publics
+#pragma mark - Privates
+#pragma mark - Delegates
+#pragma mark UITableView
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return kDefaultCellHeight;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.locations.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kLocationCellId forIndexPath:indexPath];
+    
+    CLLocation *location = self.locations[indexPath.row];
+    cell.textLabel.text = location.description;
+    cell.detailTextLabel.text = location.timestamp.description;
+    
+    return cell;
+}
+
+#pragma mark - Handlers
+
+- (void)didUpdateLocation:(NSNotification *)notification
+{
+    GVUserDefaults *defaults = [GVUserDefaults standardUserDefaults];
+    self.locations = defaults.locations;
+    
+    [self.tableView reloadData];
+}
+
+@end
